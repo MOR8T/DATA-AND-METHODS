aa7ea6e168175cc6f98ed656081824b02ee3252a
Shukurov Shahrom <112476380+MOR8T@users.noreply.github.com>
2022-10-07 17:35:53 +0500

    commit1

---
+++ main.js
@@ -0,0 +1,87 @@
+// let date = new Date();
+// console.log(date);
+
+// let date2 = new Date("2022-10-7 18:10:00:00");
+// console.log(date2);
+
+// let date3 = new Date(2022,9,6,18,10,00,00);
+// console.log(date3);
+
+
+// methods
+
+
+// get
+
+// let date = new Date();
+// console.log(date.getTime())          // getTime()
+// console.log(date.getFullYear());     // getFullYear()
+// console.log(date.getMonth());        // getMonth()        0 - 11
+// console.log(date.getDate());         // getDate()         1 - 31
+// console.log(date.getDay());          // getDay()          0 - 6
+// console.log(date.getHours());        // getHours()        0 - 23
+// console.log(date.getMinutes());      // getMinutes()      0 - 59
+// console.log(date.getSeconds());      // getSeconds()
+// console.log(date.getMilliseconds()); // getMilliseconds()
+
+
+// set 
+
+// let date = new Date();
+// console.log(date);
+// date.setFullYear(2000);  
+// console.log(date);// 2022 => 2000 
+// date.setMonth(0);
+// console.log(date);// 10 => 0
+// date.setDate(30); 
+// console.log(date);// 7 => 30 
+// date.setHours(20);
+// console.log(date);// 15 => 20
+// date.setMinutes(00); 
+// console.log(date);// 54 => 00
+// date.setSeconds(3) 
+// console.log(date);// 37 => 3
+// date.setMilliseconds(25400) 
+// console.log(date);// 000 => 25400 
+
+
+
+// UTC
+
+// let date = new Date(0);   // UTC +0
+// console.log(date);        // GMT +6
+// console.log(date); // Moscow GMT +3
+
+// getUTC
+
+
+// let date = new Date();
+// console.log(date);
+// console.log(date.getUTCFullYear());
+// console.log(date.getUTCMonth());
+// console.log(date.getUTCDate()); 
+// console.log(date.getUTCDay()); 
+// console.log(date.getUTCHours()); 
+// console.log(date.getUTCMinutes());
+// console.log(date.getUTCSeconds());
+// console.log(date.getUTCMilliseconds()); 
+
+
+// setUCT
+
+// let date = new Date();
+// console.log(date);
+// date.setUTCFullYear(2000);  
+// console.log(date);// 2022 => 2000 
+// date.setUTCMonth(0);
+// console.log(date);// 10 => 0
+// date.setUTCDate(30); 
+// console.log(date);// 7 => 30 
+// date.setUTCHours(20);
+// console.log(date);// 16 => 1
+// date.setUTCMinutes(00); 
+// console.log(date);// 35 => 00
+// date.setUTCSeconds(3) 
+// console.log(date);// 18 => 3
+// date.setUTCMilliseconds(25400) 
+// console.log(date);// 000 => 25400 
